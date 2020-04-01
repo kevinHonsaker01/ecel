b024d309456b62921a6d0b25a9ad4deb015bb98d
kevinHonsaker01 <61556777+kevinHonsaker01@users.noreply.github.com>
2020-04-01 09:23:23 -0600

    Update start_stop_collectors.py

--- start_stop_collectors.py
+++ start_stop_collectors.py
@@ -2,6 +2,7 @@ import gtk
 import signal
 from engine.engine import Engine
 from gui.main_gui import MainGUI
+import os
 
 engine = Engine()
 
@@ -10,3 +11,5 @@ if __name__ == "__main__":
     signal.signal(signal.SIGINT, signal.SIG_DFL)
     MainGUI(engine)
     gtk.main()
+    pid = os.getpid()
+    os.kill(pid, signal.SIGSTOP)
