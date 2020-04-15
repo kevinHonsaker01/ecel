f168953e3a42cb3eb703c4c641c8ba2426b4ff53
kevinHonsaker01 <61556777+kevinHonsaker01@users.noreply.github.com>
2020-04-14 21:55:38 -0600

    Update hailCesar.py

--- hailCesar.py
+++ hailCesar.py
@@ -27,22 +27,31 @@ def main():
     data = db["Demo"]
 
     ##Change this to whatever the path is for you
-    path = "C:/Users/Ben's PC/Documents/Practicum/Demo/"
+    try:
+        path = os.environ['ECEL_HOME']+'/ecel_data'
+    except:
+        print("Does not exist.")
+        pass
 
-    today = date.today()
-    today = today.strftime("%d%b%Y")
-    scenName = str(today) + "Scenario"
-    encodedFile = ""
+    if os.path.exists(path):
+        today = date.today()
+        today = today.strftime("%d%b%Y")
+        scenName = str(today) + "Scenario"
+        encodedFile = ""
 
-    scenarioStore = {"name": scenName}
+        scenarioStore = {"name": scenName}
 
-    #might need the following line for ubuntu
-    #files = glob.glob(path)
+        #might need the following line for ubuntu
+        #files = glob.glob(path)
 
-    absolutePath = ""
-    print("beginning push")
-    pushDirectory(path, scenarioStore)
-    data.insert_one(scenarioStore)
+        absolutePath = ""
+        print("beginning push")
+        pushDirectory(path, scenarioStore)
+        data.insert_one(scenarioStore)
+
+    else:
+        print("Path does not exist.")
+        sys.exit(1)
 
 if __name__ == '__main__':
     main()
