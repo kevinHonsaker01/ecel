275b06b8ca8a993ed22b4d1d2ead7d5ca428f758
kevinHonsaker01 <61556777+kevinHonsaker01@users.noreply.github.com>
2020-04-21 14:09:11 -0600

    Update collectors.sh
    April 21, 2020 -- updates to usage; removal of old data; sleeping command allowing for safe export; change ownership to allow for export/read;

--- collectors.sh
+++ collectors.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
-if [ "$EUID" -ne 0 ]; then
-    echo "ECEL must be run as root"
-    exit 1
+if [ $# -eq 0 ]; then
+    # <interval> is the time collectors will be running for; 
+    # should come from the start/stop condition of the user input;
+	echo "Usage: sudo ./collectors <interval>"
 fi
 
+SLEEP=$1
+SLEEP=$((SLEEP+5))
+
+# remove previous ecel data
+rm -rf $ECEL_HOME/ecel_data/
+
 cd "${ECEL_HOME}"
-python start_stop_collectors.py &
-sleep 15; kill -9 $(ps aux | grep start_stop_collectors | grep python | cut -d" " -f7); echo "Finished collection/zipping." &
-python3 hailCesar.py
+python start_stop_collectors.py $1 &
+sleep $SLEEP; kill -9 $(ps aux | grep start_stop_collectors | grep python | cut -d" " -f7); echo 'Finished collectors command.' 
+chown $HOSTNAME:$HOSTNAME -R ecel_data/; python3 hailCesar.py
+
+# clean up data space
+rm -rf $ECEL_HOME/ecel_data/
 exit 0
