b12b80384e62446e9bb65a63de8abb2f078e979b
kevinHonsaker01 <61556777+kevinHonsaker01@users.noreply.github.com>
2020-04-26 10:05:48 -0600

    Update start_stop_collectors.py

--- start_stop_collectors.py
+++ start_stop_collectors.py
@@ -2,14 +2,19 @@ import gtk
 import signal
 from engine.engine import Engine
 from gui.main_gui import MainGUI
-import os
+import os, sys
 
 engine = Engine()
+try:
+    interval = sys.argv[1]
+except:
+    print("Defaulting to 10 seconds.")
+    interval = 10
 
 if __name__ == "__main__":
     gtk.threads_init()
     signal.signal(signal.SIGINT, signal.SIG_DFL)
-    MainGUI(engine)
+    MainGUI(engine, interval)
     gtk.main()
     pid = os.getpid()
     os.kill(pid, signal.SIGSTOP)
