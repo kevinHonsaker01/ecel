6b6e522cf8f8a20686ef9fc403bf2c697351d968
kevinHonsaker01 <61556777+kevinHonsaker01@users.noreply.github.com>
2020-04-28 09:37:40 -0600

    Update standalone.sh

--- standalone.sh
+++ standalone.sh
@@ -6,30 +6,49 @@ if [ -z "$1" ]; then
 	echo "Usage: ./standalone.sh <interval>"
 fi
 
-# create master directory to hold all other directories
-mkdir $ECEL_HOME/ecel_data
-chown $HOSTNAME:$HOSTNAME home/
-# create directories to store the output files of each collector
-mkdir $ECEL_HOMEecel_data/snoopy; mkdir $ECEL_HOME/ecel_data/tshark; mkdir $ECEL_HOME/ecel_data/pykeylogger
+# create master directory to hold all other directories -- root owned
+cd $ECEL_HOME
+if [ -f "ecel_data"]; then
+	echo "ecel_data folder exists."
+	rm -rf ecel_data
+	mkdir ecel_data
+	mkdir ecel_data/snoopy
+	mkdir ecel_data/pykeylogger
+	mkdir ecel_data/tshark
+else
+	mkdir ecel_data
+	mkdir ecel_data/snoopy
+	mkdir ecel_data/pykeylogger
+	mkdir ecel_data/tshark
+fi
 
 # start tshark; file output
 dumpcap -i $(ifconfig | grep 1500 | cut -d":" -f1) -a duration:$1 &
+SLEEP=$1
+SLEEP=$((SLEEP+5))
 
 # start pykeylogger -- not installed ??
 
 
 # collect PIDs to stop collectors
-pid_tshark=$(ps aux | grep dumpcap | grep $(ifconfig | grep 1500 | cut -d":" -f1) | cut -d" " -f7)
+# pid_tshark=$(ps aux | grep dumpcap | grep $(ifconfig | grep 1500 | cut -d":" -f1) | cut -d" " -f7)
 # pid_pykeylogger=$()
 
 # start snoopy -- capture last because it is running the entire time after install
 cp /tmp/snoopy.log $ECEL_HOME/ecel_data/snoopy/snoopy.log
-cp /tmp/$(ll /tmp | grep wireshark | cut -d" " -f14) $ECEL_HOME/ecel_data/tshark
+sleep $SLEEP
+cp /tmp/$(ls -l /tmp | grep wireshark | cut -d" " -f14) ecel_data/tshark/
 
 # stop collectors gracefully --> they stop after a predefined condition
 
-# zip file
-zip -r ecel_data.zip $ECEL_HOME/ecel_data
+# change ownership to allow regular users to access
+chown $HOSTNAME:$HOSTNAME -R ecel_data
+
+# zip file -- root owned
+zip -r ecel_data.zip ecel_data
+
+# change ownership to allow regular users to access
+chown $HOSTNAME:$HOSTNAME ecel_data.zip
 
 # push zip to database
-python3 $ECEL_HOME/hailCesar.py 
+python3 hailCesar.py 
